efee1dc5dff075ce3a3ba1cebe9e5cd897bafaa3
Runia <967303@corp.trizetto.com>
2023-10-13 12:05:52 -0700

    adding search and maybe gitignore

---
+++ HPCProjectTemplate/Client/Pages/Home.razor.cs
@@ -0,0 +1,37 @@
+﻿using HPCProjectTemplate.Shared;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
+using System.Net.Http.Json;
+
+namespace HPCProjectTemplate.Client.Pages
+{
+    public partial class Home
+    {
+        [Inject]
+        public AuthenticationStateProvider AuthenticationStateProvider { get; set; } = null!;
+        [Inject]
+        public HttpClient Http { get; set; } = null!;
+        [Inject]
+        NavigationManager NavigationManager { get; set; }
+
+        public List<PlantList> plants { get; set; } = new List<PlantList>();
+        public string searchText { get; set; } = null!;
+        protected override async Task OnInitializedAsync()
+        {
+            var UserAuth = (await AuthenticationStateProvider.GetAuthenticationStateAsync()).User.Identity;
+            if (UserAuth is not null && UserAuth.IsAuthenticated)
+            {
+
+                //int id = plant.Id; //plant ID used for API call
+                plants = await Http.GetFromJsonAsync<List<PlantList>>($"api/search-plants?searchString=fir");
+
+
+
+            }
+        }
+        protected async Task SearchClick()
+        {
+            NavigationManager.NavigateTo("/search/" + searchText);
+        }
+    }
+}
