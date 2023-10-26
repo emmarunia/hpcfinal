2c966db7b5cf8202fa81078e63f332372c9e9b9d
cammcdonald26 <145704530+cammcdonald26@users.noreply.github.com>
2023-10-26 10:14:43 -0700

    Community Page Controller

---
+++ HPCProjectTemplate/Server/Controllers/DiscussionController.cs
@@ -0,0 +1,88 @@
+﻿using DiscussionFix.Server.Data;
+using DiscussionFix.Shared;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DiscussionFix.Server.Controllers
+{
+    public class DiscussionController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public DiscussionController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost("api/add-post")]
+        public async Task AddPost([FromBody] Posts Post)
+        {
+            _context.Posts.Add(Post);
+            _context.SaveChanges();
+        }
+
+        [HttpPost("api/reply")]
+        public async Task AddReply([FromBody] Replies Rep)
+        {
+            _context.Replies.Add(Rep);
+            _context.SaveChanges();
+        }
+
+        [HttpPost("api/add-reply-to-post")]
+        public async Task AddReplyToPost([FromBody] Replies rep)
+        {
+            var post = await (from p in  _context.Posts
+                              where p.Id == rep.PostId
+                              select p).FirstOrDefaultAsync();
+            //Replies replies = new Replies { TopicId = rep.PostId };
+
+            post.Replies.Add(rep);
+            _context.SaveChanges();
+
+            //return Ok(replies);
+        }
+
+        //sets content on discussion board home page
+        [HttpGet("api/topics")]
+        public async Task<ActionResult<TopicList>> AllTopics()
+        {
+            var board = _context.Topics
+                .Select(board => new Topics
+                {
+                    Id = board.Id,
+                    Title = board.Title,
+                    Description = board.Description
+                });
+
+            var topicList = new TopicList()
+            {
+                ListOfTopics = board
+            };
+
+            return Ok(topicList);
+        }
+
+        [HttpGet("api/get-posts")]
+        public async Task<ActionResult<PostsList>> GetPosts()
+        {
+
+            var posts = _context.Posts
+                .Select(posts => new Posts
+                {
+                    Id = posts.Id,
+                    ImageUrl = posts.ImageUrl,
+                    Title = posts.Title,
+                    Content = posts.Content,
+                    Created = posts.Created,
+                    TopicId = posts.TopicId,
+                    UserName = posts.UserName
+                });
+
+            var postList = new PostsList()
+            {
+                ListOfPosts = posts
+            };
+
+            return Ok(postList);
+        }  
+    }
+}
