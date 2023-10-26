5f454f51dc2b8542ba13edc571ed9460ed4c0ec3
cammcdonald26 <145704530+cammcdonald26@users.noreply.github.com>
2023-10-26 10:14:04 -0700

    Topics and Replies

---
+++ HPCProjectTemplate/Shared/Posts.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscussionFix.Shared
+{
+    public class Posts
+    {
+        public int Id { get; set; }
+        public string? ImageUrl { get; set; }
+        public string? Title { get; set; }
+        public string? Content { get; set; }
+        public DateTime Created { get; set; }
+        public int TopicId { get; set; }
+        public string? UserName { get; set; }
+        public List<Replies> Replies { get; set; } = new();
+    }
+}
